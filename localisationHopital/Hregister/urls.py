8c8e9446abcb7dedd1c82e401578d227e80ebab7
Jean-Kenel <jeankeneld@gmail.com>
2024-03-24 18:49:37 -0400

    Finish with Update Operation

--- localisationHopital/Hregister/urls.py
+++ localisationHopital/Hregister/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
     # http://127.0.0.1:8000/
-    path('', views.home),
+    path('', views.home, name='home'),
     path('afficher_hopitaux/', views.afficher_hopitaux, name='les-hopitaux'),
     path('ajouter_hopital/', views.ajouter_hopital, name= 'ajout-hospital'),
+    path('modifier_hopital/<int:hopital_id>/', views.modifier_hopital, name='modifier-hospital'),
 ]
 
 # DRY
--- localisationHopital/Hregister/views.py
+++ localisationHopital/Hregister/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Hopitaltracker 
 from django.views.decorators.csrf import csrf_protect
@@ -6,12 +6,12 @@ from .forms import HopitalForm
 
 # Create your views here.
 def home(request):
-    return render(request, 'hospitaux/home.html')
+    return render(request, 'hopitaux/home.html')
 
 def afficher_hopitaux(request):
     hopitaux = Hopitaltracker.objects.all()
     
-    return render(request, 'hospitaux/tous_les_hopitaux.html', {'hopitaux': hopitaux})
+    return render(request, 'hopitaux/tous_les_hopitaux.html', {'hopitaux': hopitaux})
 
 @csrf_protect
 def ajouter_hopital(request):
@@ -27,4 +27,15 @@ def ajouter_hopital(request):
         if 'submitted' in request.GET:
             submitted = True
 
-    return render(request, 'hospitaux/ajouter_un_hopital.html', {'form':form, 'submitted':submitted})
+    return render(request, 'hopitaux/ajouter_un_hopital.html', {'form':form, 'submitted':submitted})
+
+def modifier_hopital(request, hopital_id):
+    hopital = Hopitaltracker.objects.get(pk=hopital_id)
+    
+    # return HttpResponse(hopital)
+    form = HopitalForm(request.POST or None, instance=hopital)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+   
+    return render(request, 'hopitaux/modifier_un_hopital.html', {'hopital':hopital, 'form':form})
