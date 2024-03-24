6ca510d588a86db74025e47f90a4d340e8b87dd4
Jean-Kenel <jeankeneld@gmail.com>
2024-03-24 17:23:26 -0400

    Fixing the home page

--- localisationHopital/Hregister/views.py
+++ localisationHopital/Hregister/views.py
@@ -4,10 +4,9 @@ from .models import Hopitaltracker
 
 # Create your views here.
 def home(request):
-    return render(request, 'hospital/home.html')
+    return render(request, 'hospitaux/home.html')
 
 def afficher_hopitaux(request):
     hopitaux = Hopitaltracker.objects.all()
     
     return render(request, 'hospitaux/tous_les_hopitaux.html', {'hopitaux': hopitaux})
-
