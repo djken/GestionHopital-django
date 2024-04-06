a8f2db6a3d27cdb214ebe2748d56c38f8b4779e0
Jean-Kenel <jeankeneld@gmail.com>
2024-04-05 20:40:00 -0400

    Done with User Registration Extended

---
+++ localisationHopital/gestionutilisateurs/forms.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+class RegisterUserForm(UserCreationForm):
+    email = forms.EmailField()
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+        
+    def __init__(self, *args, **kwargs):
+        super(RegisterUserForm, self).__init__(*args, **kwargs)
+        
+        self.fields['first_name'].widget.attrs['class']='form-control'
+        self.fields['last_name'].widget.attrs['class']='form-control'
+        self.fields['email'].widget.attrs['class']='form-control'
+        self.fields['username'].widget.attrs['class']='form-control'
+        self.fields['password1'].widget.attrs['class']='form-control'
+        self.fields['password2'].widget.attrs['class']='form-control'
+
