180b927d37b05a8839ec83e6d1f17a10ef8e45e2
Jean-Kenel <jeankeneld@gmail.com>
2024-04-08 09:42:35 -0400

    Switch other branch

--- localisationHopital/gestionutilisateurs/views.py
+++ localisationHopital/gestionutilisateurs/views.py
@@ -4,7 +4,7 @@ from django.views.decorators.csrf import csrf_protect
 from django.contrib import messages
 
 # Create your views here.
-@csrf_protect
+# @csrf_protect
 def login_user(request):
         if request.method == "POST":
             username = request.POST['username']
@@ -21,7 +21,6 @@ def login_user(request):
         
 def logout_user(request):
     logout(request)
-
     messages.success(request,("You were logout!"))
     return redirect('home')
 
